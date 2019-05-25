e70521aa5d5fd0e75015c8ac5f3b46984c3cde98
shedoesthewoods <mmelike.mat@gmail.com>
2019-05-25 20:24:15 +0300

    first init

---
+++ Assets/LmnButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class LmnButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject lemons;
+    public Animator lemonsAnimation;
+
+    // Use this for initialization
+    void Start()
+    {
+        //vbBtnObj = GameObject.Find("VirtualButton");
+
+        lemons = GameObject.Find("rotateBtnLm");
+
+        lemons.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        lemonsAnimation = lemonsAnimation.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        lemonsAnimation.Play("lmsAnimation");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        lemonsAnimation.Play("dont");
+        Debug.Log("Button released");
+    }
+}
---
+++ Assets/SbButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SbButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject sbButton;
+    public Animator strawberryAnimation;
+
+    // Use this for initialization
+    void Start()
+    {
+        //vbBtnObj = GameObject.Find("VirtualButton");
+
+        sbButton = GameObject.Find("rotateBtnSb");
+
+        sbButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        strawberryAnimation.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        strawberryAnimation.Play("rotateAroundAnimation");
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        strawberryAnimation.Play("none");
+        Debug.Log("Button released");
+    }
+}
